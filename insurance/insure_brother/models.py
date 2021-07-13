ba461f5df71f7b740d1bbb7de8a5c436ef88c910
MarchenkoVlad <vlad.u.m.ru@gmail.com>
2021-07-13 20:03:49 +0300

    добавил модели для пользователя, компании и услуги

--- insurance/insure_brother/models.py
+++ insurance/insure_brother/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Company(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+
+class Service(models.Model):
+    id = models.AutoField(primary_key=0)
+    name = models.CharField(max_length=150)
+
+    CHOICES = [
+        ('life', 'Страхование жизни'),
+        ('health', 'Страхование здоровья'),
+        ('property', 'Страхование недвижимости'),
+        ('OSAGO', 'ОСАГО'),
+        ('CASCO', 'КАСКО')
+    ]
+    category = models.CharField(max_length=50, choices=CHOICES)
+    min_term = models.IntegerField()
+    min_payment = models.IntegerField()
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+class Customer(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30, help_text='Укажите Ваше имя')
+    lastname = models.CharField(max_length=30, help_text='Укажите Вашу фамилию')
+    phone = PhoneNumberField(null=False, blank=False, unique=True)
+    email = models.EmailField(max_length=150)
+    service = models.ForeignKey(Service, on_delete=models.SET_NULL) #как сохранить данные, если компания удалит услугу? а пользователь не должен быть удален
