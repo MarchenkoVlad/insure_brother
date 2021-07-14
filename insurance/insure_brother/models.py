178d340ea0bb7ac29f4289978f04ce854fcd9a05
MarchenkoVlad <vlad.u.m.ru@gmail.com>
2021-07-14 13:00:05 +0300

    дебажил модель

--- insurance/insure_brother/models.py
+++ insurance/insure_brother/models.py
@@ -8,7 +8,7 @@ class Company(models.Model):
 
 
 class Service(models.Model):
-    id = models.AutoField(primary_key=0)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=150)
 
     CHOICES = [
@@ -23,10 +23,11 @@ class Service(models.Model):
     min_payment = models.IntegerField()
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
+
 class Customer(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30, help_text='Укажите Ваше имя')
     lastname = models.CharField(max_length=30, help_text='Укажите Вашу фамилию')
     phone = PhoneNumberField(null=False, blank=False, unique=True)
     email = models.EmailField(max_length=150)
-    service = models.ForeignKey(Service, on_delete=models.SET_NULL) #как сохранить данные, если компания удалит услугу? а пользователь не должен быть удален
+    service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True) #как сохранить данные, если компания удалит услугу? а пользователь не должен быть
