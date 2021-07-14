f160b19a6a2815981ee6aa621f1932f3ff226471
MarchenkoVlad <vlad.u.m.ru@gmail.com>
2021-07-14 12:59:30 +0300

    подключил библиотеку для валидации номера телефона

--- insurance/insurance/settings.py
+++ insurance/insurance/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'insure_brother.apps.Insure_brotherConfig'
+    'phonenumber_field',
+    'insure_brother.apps.InsureBrotherConfig'
 ]
 
 MIDDLEWARE = [
