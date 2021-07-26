ae403b56b61f69e4fc626e9a2a31570b6c57fba2
MarchenkoVlad <vlad.u.m.ru@gmail.com>
2021-07-26 04:25:34 +0300

    добавил класс для представления главной страницы

--- insurance/insure_brother/views.py
+++ insurance/insure_brother/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from django.views.generic import ListView
 
-# Create your views here.
+from .query import get_services
+#from .filter import ServiceFilter
+from .models import Service
+
+from .form import Filtration
+
+
+class Main(ListView):
+    '''
+    Главная страница
+    '''
+    model = Service
+    template_name = 'insure_brother/home.html'
+    context_object_name = 'Service'
+
+
+    def get_queryset(self):
+        '''
+        производит филтрацию во выбранным параметрам
+        '''
+        filter = Filtration(data=self.request.GET)
+        return filter.filter(super().get_queryset())
+
+    def get_context_data(self, **kwargs):
+        '''
+        Добовляет форму для филтрации
+        '''
+        context = super(Main, self).get_context_data(**kwargs)
+        context['form'] = Filtration(data = self.request.GET)
+        return context
+
+"""
+
+def home(request):
+    services = get_services()
+    form
+    filter_backends = (DjangoFilterBackend)
+    current_filter = ServiceFilter(request.GET, queryset=Service.objects.all() )
+    context = {
+        'services': services,
+        'filter': current_filter,
+    }
+    return render(request, 'insure_brother/home.html', context)
+"""
+def admin(request):
+    pass
