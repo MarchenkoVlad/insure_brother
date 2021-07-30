b85a7f28b28314a5f05545c30df913819971ec4f
MarchenkoVlad <vlad.u.m.ru@gmail.com>
2021-07-30 11:19:24 +0300

    добавил настройки для селери

--- insurance/insurance/settings.py
+++ insurance/insurance/settings.py
@@ -133,8 +133,8 @@ LOGOUT_REDIRECT_URL = 'home'
 
 
 # setting celery
-CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
-CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
