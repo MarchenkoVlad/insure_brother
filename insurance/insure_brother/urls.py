af74b4e69199d9b3899373546a585304a81dd975
MarchenkoVlad <vlad.u.m.ru@gmail.com>
2021-08-04 18:18:51 +0300

    добавил корректный url

--- insurance/insure_brother/urls.py
+++ insurance/insure_brother/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import path, include, re_path
 
-from .views import Home, admin
+from .views import *
 
 
 urlpatterns = [
     path('home/', Home.as_view(), name='home'),
-    path('admin_company', admin, name='admin')
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', user_logout, name='logout'),
+    path('admin_company/', admin_company, name='admin_company'),
+    path('create_customer_form/', create_customer_form),
+    path('register/', Register.as_view(), name='register')
 ]
